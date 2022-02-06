1d7f81aabbe746ea4e9a1ebb0b68e48f8cc6f67a
Torogeldiev-T <tilek2402@gmail.com>
2022-02-06 17:20:54 +0600

    1253 wrong answer

---
+++ uri-midterm/src/URI1253.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class URI1253 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < line; i++) {
+            String mes = sc.next();
+            int number = sc.nextInt();
+            char p [] = mes.toCharArray();
+            for (int j = 0; j < p.length; j++) {
+                if((char) (p[j] - number) < 65) {
+                    p[j] = (char) (p[j] + 26 - number);
+                    System.out.print(p[j]);
+                }else{
+                    p[j] = (char) (p[j] - number);
+                    System.out.print(p[j]);
+                }
+            }
+        }
+    }
+}
+
+
+
