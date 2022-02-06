76a15aabaabb6690fef7c8bf3e7bfaeb79dc71f3
Torogeldiev-T <tilek2402@gmail.com>
2022-02-06 19:45:30 +0600

    1238 solved

--- uri-midterm/src/URI1238.java
+++ uri-midterm/src/URI1238.java
@@ -10,18 +10,28 @@ public class URI1238 {
             char [] c = msg.toCharArray();
             char [] p = msg1.toCharArray();
             String res ="";
-            for(int j = 0; j < c.length; j++) {
+            for(int j = 0; j < Math.min(c.length,p.length); j++) {
                     res += c[j];
                     res += p[j];
                 }
-            if(c.length > p.length){
-                String a = msg.substring(p.length , c.length);
-                res += a;
+            //method with loop
+            for (int j = Math.min(p.length,c.length) ; j < Math.max(p.length, c.length);j++){
+                if(c.length > p.length){
+                    res += c[j];
+                }else {
+                    res += p[j];
+                }
             }
-            if(p.length > c.length){
-                String a = msg1.substring(c.length ,p.length);
-                res += a;
-            }
+
+// method with substring
+//            if(c.length > p.length){
+//                String a = msg.substring(p.length , c.length);
+//                res += a;
+//            }
+//            if(p.length > c.length){
+//                String a = msg1.substring(c.length ,p.length);
+//                res += a;
+//            }
             System.out.println(res);
         }
     }
