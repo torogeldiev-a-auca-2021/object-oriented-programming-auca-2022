776af973c72c98ebb91c7db57d413b0d66608b5b
Torogeldiev-T <tilek2402@gmail.com>
2022-02-08 01:26:17 +0600

    1241 Time limit exceeded

--- uri-midterm/src/URI1241.java
+++ uri-midterm/src/URI1241.java
@@ -4,26 +4,48 @@ public class URI1241 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int lines = sc.nextInt();
-        String res ="";
+        String res = "";
 
-        for ( int i = 0; i < lines; i++){
+        for (int i = 0; i < lines; i++) {
             String msg = sc.next();
             String message = sc.next();
-            for (int j = 0; j < msg.length(); j++){
-                for(int p = 0; p < message.length();p++){
-                    char a = msg.charAt(msg.length() - 1);
-                    char b = message.charAt(message.length() - 1);
-                    if(a == b){
+            char[] a = msg.toCharArray();
+            char[] b = message.toCharArray();
+            for (int j = 0; j < a.length; j++) {
+                for (int p = 0; p < b.length; p++) {
+                    if (a.length > b.length) {
+                        String r = msg.substring(a.length - b.length, a.length);
+                        if (r.equals(message)) {
+                            res = "encaixa";
+                        } else {
+                            res = "nao encaixa";
+                        }
+                    }
+                }
+                if(b.length > a.length){
+                    String t = message.substring(b.length - a.length, b.length);
+                    if (t.equals(message)) {
                         res = "encaixa";
+                    } else {
+                        res = "nao encaixa";
+                    }
 
+                }
+                if(a.length == b.length){
+                    if(msg.equals(message)){
+                        res = "encaixa";
                     }else{
                         res = "nao encaixa";
-
-
                     }
                 }
             }
             System.out.println(res);
-            }
+        }
     }
 }
+
+
+
+
+
+
