2dea7ffede9383f9d475b1e172ab00c129c2f949
Torogeldiev-T <tilek2402@gmail.com>
2022-03-01 00:34:03 +0600

    solved

---
+++ uri-midterm/src/URI1871.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class URI1871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        while (sc.hasNextLine()) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            int result = a + b;
+            String str = Integer.toString(result);
+            String replace = str.replace("0", "");
+            System.out.println(replace);
+
+
+
+        }
+    }
+}
--- uri-midterm/src/URI2242.java
+++ uri-midterm/src/URI2242.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class URI1242 {
+public class URI2242 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String vowels = sc.next();
