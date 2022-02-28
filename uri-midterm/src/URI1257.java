2deaba8518d44fcac3e7cdcd8b1a49ff411b2444
Torogeldiev-T <tilek2402@gmail.com>
2022-03-01 02:16:48 +0600

    solved

--- uri-midterm/src/URI1257.java
+++ uri-midterm/src/URI1257.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class URI1257 {
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        for (int i = 0 ; i < a ; i++){
-            String msg = sc.nextLine();
-            char [] chars = msg.toCharArray();
-            if(chars)
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            extracted(sc);
         }
+    }
+
+    private static void extracted(Scanner sc) {
+        int num2 = sc.nextInt();
+        int result = 0;
+        for (int j = 0; j < num2; j++) {
+            String msg = sc.next();
+            for (int i = 0; i < msg.length(); i++) {
+                result += msg.charAt(i) - 65 + i + j;
+            }
+        }
+        System.out.println(result);
     }
 }
