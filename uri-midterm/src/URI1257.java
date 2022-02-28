ab00714885dcc29a70a63d2ee4300de17e46a42f
Torogeldiev-T <tilek2402@gmail.com>
2022-03-01 02:13:09 +0600

    solved

---
+++ uri-midterm/src/URI1257.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class URI1257 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        for (int i = 0 ; i < a ; i++){
+            String msg = sc.nextLine();
+            char [] chars = msg.toCharArray();
+            if(chars)
+        }
+    }
+}
---
+++ uri-midterm/src/URI1273.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class URI1273 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int stop;
+        while (sc.hasNextLine()){
+            stop = sc.nextInt();
+            if(stop == 0){
+                break;
+            }
+            for(int i = 0 ; i < stop ; i++ ){
+                String msg = sc.nextLine();
+
+            }
+        }
+    }
+}
