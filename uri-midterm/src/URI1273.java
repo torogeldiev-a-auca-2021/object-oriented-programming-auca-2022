5e5aebbe9d0a69a38227a157e361a05d3396dad0
Torogeldiev-T <tilek2402@gmail.com>
2022-03-08 03:14:43 +0600

    1273 accepted

--- uri-midterm/src/URI1273.java
+++ uri-midterm/src/URI1273.java
@@ -3,13 +3,29 @@ import java.util.Scanner;
 public class URI1273 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        while(n != 0){
-            for(int i = 0; i < n ; i++){
-                String msg = sc.nextLine();
-
+        int lines = sc.nextInt();
+        while (lines > 0) {
+            String names[] = new String[lines + 1];
+            names[0] = "";
+            for (int i = 1; i < lines + 1; i++) {
+                names[i] = sc.next();
             }
+            for (int j = 0; j < names.length; j++) {
+                if (names[j].length() > names[0].length()) {
+                    names[0] = names[j];
+                }
+            }
+            for (int j = 1; j < names.length; j++) {
+                for (int i = 0; i < names[0].length() - names[j].length(); i++) {
+                    System.out.print(" ");
+                }
+                System.out.println(names[j]);
+            }
+            lines = sc.nextInt();
+            if (lines == 0)
+                break;
+            else
+                System.out.println();
         }
     }
 }
