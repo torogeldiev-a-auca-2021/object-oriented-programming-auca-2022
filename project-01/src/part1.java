7a51978a2566550d724fc8113e1ae4a138d13f13
Torogeldiev-T <tilek2402@gmail.com>
2022-05-12 15:27:44 +0600

    first project

--- project-01/src/part1.java
+++ project-01/src/part1.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 public class part1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Game2048  game = new Game2048();
+
         int [][] board = new int[4][4];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 int x = sc.nextInt();
                 board[i][j] = x;
-                System.out.printf("%5d", board[i][j]);
-            }
-            System.out.println();
-        }
-
-    }
-
-    public void  print(int row , int col, int num){
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-
             }
         }
-
     }
-}
+    }
+
+        class Game2048 {
+
+            public int write[][] = new int[4][4];
+
+            private void print(int row, int col, int value){
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 4; j++) {
+
+                    }
+                }
+        }
+    }
+
 
